cc1870da1abac7f8d40fa5b075bf2774697fec3d
mohamedali1995 <muhamedali166@gmail.com>
2019-02-18 01:35:56 +0200

    Reformatting file.

--- src/main.py
+++ src/main.py
@@ -3,16 +3,6 @@ import numpy as np
 from sympy import mod_inverse
 
 
-def xgcd(a, b):
-    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
-    x0, x1, y0, y1 = 0, 1, 1, 0
-    while a != 0:
-        q, b, a = b // a, a, b % a
-        y0, y1 = y1, y0 - q * y1
-        x0, x1 = x1, x0 - q * x1
-    return x0
-
-
 def generate_keys(n_bits):
     while True:
         p = crypto.getPrime(n_bits)
